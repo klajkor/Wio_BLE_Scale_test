152c0482210d3caf9a7ace881a3e7019f1f8dac9
Robert Klajko <robert_klajko@epam.com>
2021-07-30 21:10:32 +0200

    Code refactored, reconnect in 2nd round still doesn't work

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wio_BLE_Scale_test
--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@
  * updated by chegewara
  */
 
+#if defined(__SAMD51__)
+// for Wio Terminal:
 #include "rpcBLEDevice.h"
-//#include <BLEScan.h>
-//#include <BLEAdvertisedDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
 #include "seeed_rpcUnified.h"
 #include "rtl_ble/ble_unified.h"
 #include "rtl_ble/ble_client.h"
+#else
 // for ESP32:
-//#include "BLEDevice.h"
-//#include <BLEScan.h>
-//#include <BLEAdvertisedDevice.h>
+#include "BLEDevice.h"
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#endif
 
 #define SCALE_MAIN_SERVICE_UUID "0000FFF0-0000-1000-8000-00805F9B34FB"
 #define SCALE_READ_CHAR_UUID "0000FFF4-0000-1000-8000-00805F9B34FB"
@@ -23,8 +27,10 @@
 
 #define SCALE_PNP_SERVICE_UUID "0000180A-0000-1000-8000-00805F9B34FB"
 #define SCALE_PNP_CHAR_UUID "00002A50-0000-1000-8000-00805F9B34FB"
-// The remote service we wish to connect to.
-//static BLEUUID serviceUUID(0xFEE0);
+
+#define MAX_NOTIFY_READ_CYCLE 100U
+
+// Scale main service UUID
 static BLEUUID serviceUUID(SCALE_MAIN_SERVICE_UUID);
 // The characteristic of the remote service we are interested in.
 static BLEUUID charUUID_RD(SCALE_READ_CHAR_UUID);
@@ -33,15 +39,19 @@ static BLEUUID charUUID_WR(SCALE_WRITE_CHAR_UUID);
 static boolean doConnect = true;
 static boolean connected = false;
 static boolean doScanRestart = false;
+static boolean callBackRegistered = false;
 static BLEScan *pBLEScan;
-static BLERemoteCharacteristic *pRemoteCharacteristic;
+static BLERemoteCharacteristic *pReadCharacteristic;
+static BLERemoteCharacteristic *pWriteCharacteristic;
 static BLEAdvertisedDevice *myDevice;
 #if defined(__SAMD51__)
-uint8_t bd_addr[6] = {0x1C, 0x03, 0x00, 0x10, 0xFF, 0xFF};
+uint8_t scale_BT_MAC_addr[6] = {0x1C, 0x03, 0x00, 0x10, 0xFF, 0xFF};
 #else
-uint8_t bd_addr[6] = {0xFF, 0xFF, 0x10, 0x00, 0x03, 0x1C};
+uint8_t scale_BT_MAC_addr[6] = {0xFF, 0xFF, 0x10, 0x00, 0x03, 0x1C};
 #endif
-BLEAddress DecentScaleAddr(bd_addr);
+BLEAddress DecentScaleAddr(scale_BT_MAC_addr);
+BLEClient *pScaleClient;
+BLERemoteService *pScaleRemoteService;
 
 uint8_t cmd_LedOn[7] = {0x03, 0x0A, 0x01, 0x01, 0x00, 0x00, 0x09};
 uint8_t cmd_LedOff[7] = {0x03, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x09};
@@ -109,35 +119,36 @@ static void notifyCallback(
   Serial.print(" of data length ");
   Serial.println(length);
   */
-  if (call_back_counter < 50)
+  if (call_back_counter < MAX_NOTIFY_READ_CYCLE)
   {
     call_back_counter++;
-
-    Serial.print("CB data ");
-    if (call_back_counter < 10)
+    if (call_back_counter % 10 == 0)
     {
-      Serial.print("0");
+      Serial.print("Scale data #");
+      Serial.print(call_back_counter);
+      Serial.print(": ");
+      Serial_println_chars_in_hex(pData, length);
     }
-    Serial.print(call_back_counter);
-    Serial.print(":");
-    Serial_println_chars_in_hex(pData, length);
   }
   else
   {
     pBLERemoteCharacteristic->registerForNotify(nullptr);
+    delay(100);
+    pBLERemoteCharacteristic->registerForNotify(nullptr);
+    callBackRegistered = false;
     Serial.print("Call back unregistered.");
   }
 }
 
 class MyClientCallback : public BLEClientCallbacks
 {
-  void onConnect(BLEClient *pclient)
+  void onConnect(BLEClient *pCBclient)
   {
     connected = true;
     Serial.println("onConnect method");
   }
 
-  void onDisconnect(BLEClient *pclient)
+  void onDisconnect(BLEClient *pCBclient)
   {
     connected = false;
     Serial.println("onDisconnect method");
@@ -149,26 +160,31 @@ bool connectToServer()
 {
   uint8_t read_cycle;
   std::string read_value = "123456789012345678901";
+  bool doWriteCmd;
 
-  BLEClient *pClient = BLEDevice::createClient();
-  if (pClient == nullptr)
+  doWriteCmd = false;
+  pScaleClient = BLEDevice::getClient();
+  if (pScaleClient == nullptr)
   {
-    Serial.println("Failed to create a BLE client");
+    pScaleClient = BLEDevice::createClient();
+    pScaleClient->setClientCallbacks(new MyClientCallback());
+    doWriteCmd = true;
+  }
+  if (pScaleClient == nullptr)
+  {
+    Serial.println("Failed to create a new BLE client");
     return false;
   }
-  Serial.println("Created a BLE client");
   Serial.print("Forming a connection to ");
   Serial.println(myDevice->getAddress().toString().c_str());
   //Serial.println(DecentScaleAddr.toString().c_str());
 
-  pClient->setClientCallbacks(new MyClientCallback());
-
-  // Connect to the remove BLE Server.
-  pClient->connect(myDevice); // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-  //pClient->connect(DecentScaleAddr);
-
   delay(100);
-  if (pClient->isConnected())
+  // Connect to the remove BLE Server.
+  pScaleClient->connect(myDevice); // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  //pScaleClient->connect(DecentScaleAddr);
+  delay(100);
+  if (pScaleClient->isConnected())
   {
     Serial.println(" - Connected to server");
   }
@@ -178,72 +194,72 @@ bool connectToServer()
     return false;
   }
   // Obtain a reference to the service we are after in the remote BLE server.
+  delay(200);
+  Serial.print(F("Searching for service: "));
   Serial.println(serviceUUID.toString().c_str());
-  BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
-  //BLERemoteService *pRemoteService = pClient->getService(SCALE_MAIN_SERVICE_UUID);
-  if (pRemoteService == nullptr)
+  pScaleRemoteService = nullptr;
+  pScaleRemoteService = pScaleClient->getService(serviceUUID);
+  if (pScaleRemoteService == nullptr)
   {
-    Serial.print("Failed to find our service UUID: ");
-    Serial.println(serviceUUID.toString().c_str());
-    pClient->disconnect();
+    Serial.print(F(" - Failed to find our service, disconnecting."));
+    pScaleClient->disconnect();
     return false;
   }
-  Serial.println(" - Found our service");
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID_WR);
-  if (pRemoteCharacteristic == nullptr)
+  Serial.println(F(" - Found our service"));
+  if (doWriteCmd)
   {
-    Serial.print("Failed to find our Write characteristic UUID: ");
+    Serial.print(F("Searching for Write characteristic: "));
     Serial.println(charUUID_WR.toString().c_str());
-    pClient->disconnect();
-    return false;
-  }
-  else
-  {
-    Serial.println(" - Found our Write characteristic");
-    if (pRemoteCharacteristic->canWrite())
+    pWriteCharacteristic = pScaleRemoteService->getCharacteristic(charUUID_WR);
+    if (pWriteCharacteristic == nullptr)
     {
-      Serial.println(" -  and it is writeable");
-      Serial.println(F("Sending Led OFF command to scale "));
-      pRemoteCharacteristic->writeValue(cmd_LedOff, 7, false);
-      delay(500);
-      pRemoteCharacteristic->writeValue(cmd_LedOff, 7, false);
-      delay(500);
-      Serial.println(F("Sending Led ON command to scale "));
-      pRemoteCharacteristic->writeValue(cmd_LedOn, 7, false);
-      delay(500);
-      pRemoteCharacteristic->writeValue(cmd_LedOn, 7, false);
-      delay(500);
-      Serial.println(F("Sending Timer Stop & Reset command to scale "));
-      pRemoteCharacteristic->writeValue(cmd_TimerStop, 7, false);
-      delay(500);
-      pRemoteCharacteristic->writeValue(cmd_TimerStop, 7, false);
-      delay(500);
-      pRemoteCharacteristic->writeValue(cmd_TimerReset, 7, false);
-      delay(500);
-      pRemoteCharacteristic->writeValue(cmd_TimerReset, 7, false);
-      delay(500);
+      Serial.print(F("Failed to find our Write characteristic"));
+    }
+    else
+    {
+      Serial.println(F(" - Found our Write characteristic"));
+      if (pWriteCharacteristic->canWrite())
+      {
+        Serial.println(F(" -  and it is writeable"));
+        Serial.println(F("Sending double Led OFF command to scale "));
+        pWriteCharacteristic->writeValue(cmd_LedOff, 7, false);
+        delay(300);
+        pWriteCharacteristic->writeValue(cmd_LedOff, 7, false);
+        delay(300);
+        Serial.println(F("Sending double Led ON command to scale "));
+        pWriteCharacteristic->writeValue(cmd_LedOn, 7, false);
+        delay(300);
+        pWriteCharacteristic->writeValue(cmd_LedOn, 7, false);
+        delay(300);
+        Serial.println(F("Sending Timer Stop & Reset command to scale "));
+        pWriteCharacteristic->writeValue(cmd_TimerStop, 7, false);
+        delay(300);
+        pWriteCharacteristic->writeValue(cmd_TimerReset, 7, false);
+        delay(300);
+      }
     }
   }
 
   // Obtain a reference to the characteristic in the service of the remote BLE server.
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID_RD);
-  if (pRemoteCharacteristic == nullptr)
+  Serial.print(F("Searching for Read characteristic: "));
+  Serial.println(charUUID_RD.toString().c_str());
+  pReadCharacteristic = pScaleRemoteService->getCharacteristic(charUUID_RD);
+  if (pReadCharacteristic == nullptr)
   {
-    Serial.print("Failed to find our Read characteristic UUID: ");
-    Serial.println(charUUID_RD.toString().c_str());
-    pClient->disconnect();
+    Serial.print("Failed to find our Read characteristic, disconnecting");
+    pScaleClient->disconnect();
     return false;
   }
   Serial.println(" - Found our Read characteristic");
 
   // Read the value of the characteristic.
-  if (pRemoteCharacteristic->canRead())
+  if (pReadCharacteristic->canRead())
   {
-    Serial.println(" - reading started");
+    Serial.println(" - test reading started");
     read_cycle = 0;
-    while (read_cycle < 10)
+    while (read_cycle < 5)
     {
-      read_value = pRemoteCharacteristic->readValue();
+      read_value = pReadCharacteristic->readValue();
       if (read_value.at(0) != 3)
       {
         read_cycle = 10;
@@ -255,37 +271,30 @@ bool connectToServer()
     }
   }
 
-  if (pRemoteCharacteristic->canNotify())
+  if (pReadCharacteristic->canNotify())
   {
     Serial.println(" - Characteristic can notify");
     call_back_counter = 0;
-    pRemoteCharacteristic->registerForNotify(notifyCallback);
+    callBackRegistered = true;
+    pReadCharacteristic->registerForNotify(notifyCallback);
+    delay(200);
   }
-  Serial.println("delay(6000)");
-  delay(2000);
-  delay(2000);
-  delay(2000);
   /*
   Serial.println("Disconnecting");
-  pClient->disconnect();
+  pScaleClient->disconnect();
   connected = false;
   read_cycle = 0;
   */
-  while (pClient->isConnected())
+  while (callBackRegistered && call_back_counter < MAX_NOTIFY_READ_CYCLE)
   {
-    if (pRemoteCharacteristic->canRead())
-    {
-      read_value = pRemoteCharacteristic->readValue();
-      Serial.print("Value: ");
-      Serial_println_string_in_hex(&read_value);
-    }
-    else
-    {
-      Serial.print(".");
-    }
-    delay(1000);
-    read_cycle++;
+    delay(1);
   }
+  pWriteCharacteristic->~BLERemoteCharacteristic();
+  pReadCharacteristic->~BLERemoteCharacteristic();
+  pScaleClient->disconnect();
+  delay(300);
+  pScaleClient->disconnect();
+  delay(300);
   Serial.println("Disconnected");
   doScanRestart = true;
   return false;
@@ -306,17 +315,16 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
     // We have found a device, let us now see if it contains the service we are looking for.
     if (memcmp(advertisedDevice.getAddress().getNative(), DecentScaleAddr.getNative(), 6) == 0)
     {
-      Serial.print("BATT Client Device found: ");
+      Serial.print("Decent Scale found: ");
       Serial.println(advertisedDevice.toString().c_str());
       Serial.print("Address Type: ");
       Serial.println(advertisedDevice.getAddressType());
       BLEDevice::getScan()->stop();
       /*
       */
-      //Serial.println("new BLEAdvertisedDevice");
       myDevice = new BLEAdvertisedDevice(advertisedDevice);
-      //Serial.println("new BLEAdvertisedDevice done");
       doConnect = true;
+      doScanRestart = true;
     } // onResult
     else
     {
@@ -333,7 +341,7 @@ void setup()
   };
   delay(2000);
   Serial.println("Starting Arduino BLE Client application...");
-  BLEDevice::init("Scale_Client");
+  BLEDevice::init("Wio_Scale_Client");
 
   // Retrieve a Scanner and set the callback we want to use to be informed when we
   // have detected a new device.  Specify that we want active scanning and start the
@@ -343,7 +351,7 @@ void setup()
   pBLEScan->setInterval(1349);
   pBLEScan->setWindow(449);
   pBLEScan->setActiveScan(true);
-  pBLEScan->start(25, true);
+  pBLEScan->start(5, false);
   /*
   */
 
@@ -366,6 +374,7 @@ void loop()
     else
     {
       Serial.println("We are now disconnected from the BLE Server.");
+      delay(1000);
       doConnect = true;
     }
     if (doScanRestart == true)
