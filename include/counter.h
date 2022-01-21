f75783e4c34043cde1485b56ad90056fd9b63ae9
Robert Klajko <klajkor@gmail.com>
2022-01-21 23:34:55 +0100

    Counter state machine moved to a dedicated module

--- include/counter.h
+++ include/counter.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <Arduino.h>
-#include "main_defs.h"
+#include "Arduino.h"
+#include "decent_scale.h"
 #include "display.h"
+#include "main_defs.h"
 
 typedef enum
 {
@@ -13,13 +14,13 @@ typedef enum
     COUNTER_STATE_STOP
 } counter_states_e;
 
-//SM Counter variables
-extern int state_counter1;      //The actual state of the state machine
-extern int prev_state_counter1; //Remembers the previous state (useful to know when the state has changed)
-extern int iSecCounter1;
-extern int prev_iSecCounter1;
-extern int iMinCounter1;
-extern int prev_iMinCounter1;
+// SM Counter variables
+extern int           state_counter1;      // The actual state of the state machine
+extern int           prev_state_counter1; // Remembers the previous state (useful to know when the state has changed)
+extern int           iSecCounter1;
+extern int           prev_iSecCounter1;
+extern int           iMinCounter1;
+extern int           prev_iMinCounter1;
 extern unsigned long start_counter1;
 extern unsigned long elapsed_counter1;
 
--- src/counter.cpp
+++ src/counter.cpp
@@ -1,23 +1,24 @@
 #include "counter.h"
 
-//SM Counter variables
-int state_counter1 = COUNTER_STATE_RESET;      //The actual state of the state machine
-int prev_state_counter1 = COUNTER_STATE_RESET; //Remembers the previous state (useful to know when the state has changed)
-int iSecCounter1 = -1;
-int prev_iSecCounter1 = 0;
-int iMinCounter1 = -1;
-int prev_iMinCounter1 = 0;
+// SM Counter variables
+int state_counter1 = COUNTER_STATE_RESET; // The actual state of the state machine
+int prev_state_counter1 =
+    COUNTER_STATE_RESET; // Remembers the previous state (useful to know when the state has changed)
+int           iSecCounter1 = -1;
+int           prev_iSecCounter1 = 0;
+int           iMinCounter1 = -1;
+int           prev_iMinCounter1 = 0;
 unsigned long start_counter1 = 0;
 unsigned long elapsed_counter1 = 0;
 
 /**
-* @brief Counter 1 state machine - counts the seconds
-*/
+ * @brief Counter 1 state machine - counts the seconds
+ */
 void StateMachine_counter1(float weight_diff_i, float min_diff_treshold)
 {
     prev_state_counter1 = state_counter1;
 
-    //State Machine Section
+    // State Machine Section
     switch (state_counter1)
     {
     case COUNTER_STATE_RESET:
@@ -38,8 +39,8 @@ void StateMachine_counter1(float weight_diff_i, float min_diff_treshold)
         break;
     case COUNTER_STATE_START:
         state_counter1 = COUNTER_STATE_COUNTING;
+        //decent_cmd_timer_start();
         wio_brew_timer_update(iSecCounter1);
-        //state_Display = DISPLAY_STATE_TIMER_RUNNING;
         break;
     case COUNTER_STATE_COUNTING:
         prev_iSecCounter1 = iSecCounter1;
@@ -50,14 +51,14 @@ void StateMachine_counter1(float weight_diff_i, float min_diff_treshold)
         {
             wio_brew_timer_update(iSecCounter1);
         }
-        if (iSecCounter1 > 0 && weight_diff_i < min_diff_treshold)
+        if (weight_diff_i < min_diff_treshold)
         {
             state_counter1 = COUNTER_STATE_STOP;
         }
         break;
     case COUNTER_STATE_STOP:
         state_counter1 = COUNTER_STATE_DISABLED;
-        //state_Display = DISPLAY_STATE_TIMER_STOPPED;
+        //decent_cmd_timer_stop();
         wio_brew_timer_update(iSecCounter1);
         break;
     }
