c0220e706a2d5ced47782dea19c153603e5acd83
Robert Klajko <klajkor@gmail.com>
2022-01-23 00:22:32 +0100

    Message strings separated to a header file

---
+++ include/app_messages.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "Arduino.h"
+
+const char MSG_SETUP_STARTED[] = "Setup started.";
+const char MSG_START_BLE_APP[] = "Starting Arduino BLE Client application...";
+const char MSG_SCANNING[] = "Scanning";
+const char MSG_DECENT_SCALE_FOUND[] = "Decent Scale found";
+const char MSG_CONNECTED_TO_BLE_SERVER[] = "Connected to the BLE Server.";
+const char MSG_NOT_CONNECTED_TO_BLE_SERVER[] = "NOT connected to the BLE Server.";
+const char MSG_BLE_SCAN_RESTART[] = "BLE Scan re-start";
+const char MSG_ONCONNECT_METHOD[] = "onConnect method";
+const char MSG_ONDISCONNECT_METHOD[] = "onDisconnect method";
+const char MSG_DISCONNECTED[] = "Disconnected.";
+const char MSG_CONNECTING_TO_SCALE[] = "Connecting to scale...";
+const char MSG_FAILED_TO_CREATE_BLE_CLIENT[] = "Failed to create a new BLE client";
+const char MSG_FORMING_CONNECTION_TO[] = "Forming a connection to";
+const char MSG_CONNECTION_FAILED[] = "Connection failed!";
+const char MSG_SEARCHING_FOR[] = "Searching for ";
+const char MSG_SERVICE[] = "service ";
+const char MSG_FAILED_TO_FIND[] = "Failed to find ";
+const char MSG_DISCONNECTING[] = "disconnecting";
+const char MSG_FOUND[] = "Found ";
+const char MSG_WRITE_CHARACTERISTIC[] = "Write characteristic ";
+const char MSG_WRITEABLE[] = " - writeable";
+const char MSG_NOT_WRITEABLE[] = " - NOT writeable";
+const char MSG_READ_CHARACTERISTIC[] = "Read characteristic ";
+const char MSG_TEST_READING_STARTED[] = " - test reading started";
