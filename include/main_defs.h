d77fdde7ad2ae539e5a665fcd9ac30684b3ce88d
Robert Klajko <klajkor@gmail.com>
2022-01-23 00:22:05 +0100

    refactor

--- include/main_defs.h
+++ include/main_defs.h
@@ -1,13 +1,7 @@
 #pragma once
 
-#include <string.h>
-
 #include "Arduino.h"
 
-#define MAX_NOTIFY_READ_CYCLE 64000U
+#define ENABLE_DEBUG_PRINT (1)
 #define MIN_WEIGHT_INC ((float)0.2)
 
-void tap_counter(void);
-void wio_accelerometer_init(void);
-void wio_get_acceleroXYZ_str(char *pAcceleroStr_o);
-void wio_gpio_init(void);
