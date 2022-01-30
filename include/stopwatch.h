0468b238186f983280de751909b655da69d749d2
Robert Klajko <klajkor@gmail.com>
2022-01-30 01:01:25 +0100

    Stopwatch refactored

--- include/stopwatch.h
+++ include/stopwatch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Arduino.h"
+#include "app_timer.h"
 #include "decent_scale.h"
 #include "display.h"
 #include "main_defs.h"
@@ -8,19 +9,13 @@
 typedef enum
 {
     STOPWATCH_RESET = 0,
+    STOPWATCH_STARTING,
     STOPWATCH_RUNNING,
+    STOPWATCH_STOPPING,
     STOPWATCH_STOPPED,
     STOPWATCH_NO_CHANGE
 } stopwatch_states_t;
 
-typedef struct
-{
-    uint32_t start_time;
-    uint32_t secs;
-    uint32_t mins;
-    bool     running;
-} StopwatchTimer_st;
-
-extern StopwatchTimer_st stopwatch_timer;
+extern AppTimer_st stopwatch_timer;
 
 stopwatch_states_t fsm_stopwatch(stopwatch_states_t new_state);
--- src/stopwatch.cpp
+++ src/stopwatch.cpp
@@ -1,6 +1,6 @@
 #include "stopwatch.h"
 
-StopwatchTimer_st stopwatch_timer = {.start_time = 0, .secs = 0, .mins = 0, .running = false};
+AppTimer_st stopwatch_timer = {.start_time = 0, .secs = 0, .mins = 0, .running = false};
 
 static stopwatch_states_t stopwatch_state = STOPWATCH_NO_CHANGE;
 static stopwatch_states_t stopwatch_prev_state = STOPWATCH_NO_CHANGE;
@@ -9,30 +9,41 @@ stopwatch_states_t fsm_stopwatch(stopwatch_states_t new_state)
 {
     uint32_t elapsed_time;
     uint32_t prev_secs;
-    switch (new_state)
+    if (new_state != STOPWATCH_NO_CHANGE)
+    {
+        stopwatch_state = new_state;
+    }
+    switch (stopwatch_state)
     {
     case STOPWATCH_RESET:
-        if (stopwatch_state != new_state)
+        stopwatch_timer.start_time = 0;
+        stopwatch_timer.secs = 0;
+        stopwatch_timer.mins = 0;
+        stopwatch_timer.running = false;
+        wio_brew_timer_update(stopwatch_timer.secs, stopwatch_timer.mins);
+        if (stopwatch_prev_state == STOPWATCH_RUNNING)
         {
-            stopwatch_state = new_state;
-            stopwatch_prev_state = stopwatch_state;
-            stopwatch_timer.start_time = 0;
-            stopwatch_timer.secs = 0;
-            stopwatch_timer.mins = 0;
-            stopwatch_timer.running = false;
-            wio_brew_timer_update(stopwatch_timer.secs, stopwatch_timer.mins);
+            decent_cmd_timer_stop();
         }
-        //decent_cmd_timer_reset();
+        decent_cmd_timer_reset();
+        stopwatch_prev_state = stopwatch_state;
+        stopwatch_state = STOPWATCH_NO_CHANGE;
         break;
-    case STOPWATCH_RUNNING:
-        if (stopwatch_state != new_state)
+    case STOPWATCH_STARTING:
+        if (stopwatch_prev_state == STOPWATCH_RESET || stopwatch_prev_state == STOPWATCH_NO_CHANGE)
         {
-            stopwatch_state = new_state;
-            stopwatch_prev_state = stopwatch_state;
             stopwatch_timer.running = true;
             stopwatch_timer.start_time = millis();
-            //decent_cmd_timer_start();
+            decent_cmd_timer_start();
+            stopwatch_prev_state = stopwatch_state;
+            stopwatch_state = STOPWATCH_RUNNING;
         }
+        else
+        {
+            stopwatch_state = stopwatch_prev_state;
+        }
+        break;
+    case STOPWATCH_RUNNING:
         prev_secs = stopwatch_timer.secs;
         elapsed_time = millis() - stopwatch_timer.start_time;
         stopwatch_timer.secs = uint32_t((elapsed_time / 1000) % 60);
@@ -41,18 +52,27 @@ stopwatch_states_t fsm_stopwatch(stopwatch_states_t new_state)
         {
             wio_brew_timer_update(stopwatch_timer.secs, stopwatch_timer.mins);
         }
+        stopwatch_prev_state = stopwatch_state;
         break;
-    case STOPWATCH_STOPPED:
-        if (stopwatch_state != new_state)
+    case STOPWATCH_STOPPING:
+        if (stopwatch_prev_state == STOPWATCH_RUNNING)
         {
-            stopwatch_state = new_state;
-            stopwatch_prev_state = stopwatch_state;
             stopwatch_timer.running = false;
             wio_brew_timer_update(stopwatch_timer.secs, stopwatch_timer.mins);
+            decent_cmd_timer_stop();
+            stopwatch_prev_state = stopwatch_state;
+            stopwatch_state = STOPWATCH_STOPPED;
         }
-        //decent_cmd_timer_stop();
+        else
+        {
+            stopwatch_state = stopwatch_prev_state;
+        }
+        break;
+    case STOPWATCH_STOPPED:
+        stopwatch_prev_state = stopwatch_state;
         break;
     case STOPWATCH_NO_CHANGE:
+        stopwatch_prev_state = stopwatch_state;
         break;
     }
     return stopwatch_state;
