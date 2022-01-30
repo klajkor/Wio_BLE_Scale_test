8c0d7373973ebb3c26bbad9ad69b596b87ffbda0
Robert Klajko <klajkor@gmail.com>
2022-01-30 01:00:20 +0100

    Function definitions for main.cpp

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Arduino.h"
+
+// Function defs for main.cpp only
+bool connectToServer();
+void setup(void);
+void loop(void);
+void scale_read_timer_callback(TimerHandle_t xTimer);
+void battery_status_update_callback(TimerHandle_t xTimer);
