f294164ae9f229bb67b1d3f5e49ecc3d74fe232f
Robert Klajko <klajkor@gmail.com>
2025-01-03 22:40:27 +0100

    GPIO button functions moved to dedicated files

--- include/wio_gpio.h
+++ include/wio_gpio.h
@@ -4,4 +4,12 @@
 
 #include "Arduino.h"
 
+extern uint32_t KEY_STOPWATCH;
+extern uint32_t KEY_SCALE_TARE;
+
+extern bool key_stopwatch_pressed;
+extern bool key_scale_tare_pressed;
+
 void wio_gpio_init(void);
+void key_stopwatch_handler(void);
+void key_scale_handler(void);
--- src/wio_gpio.cpp
+++ src/wio_gpio.cpp
@@ -1,13 +1,31 @@
 #include "wio_gpio.h"
 
+#if defined(__SAMD51__)
+uint32_t KEY_STOPWATCH = WIO_KEY_A;
+uint32_t KEY_SCALE_TARE = WIO_KEY_C;
+#endif
+
+bool key_stopwatch_pressed = false;
+bool key_scale_tare_pressed = false;
+
 void wio_gpio_init(void)
 {
 #if defined(__SAMD51__)
-    pinMode(WIO_KEY_A, INPUT_PULLUP);
-    pinMode(WIO_KEY_B, INPUT_PULLUP);
-    pinMode(WIO_KEY_C, INPUT_PULLUP);
+    pinMode(KEY_STOPWATCH, INPUT_PULLUP);
+    pinMode(KEY_SCALE_TARE, INPUT_PULLUP);
     pinMode(WIO_5S_PRESS, INPUT_PULLUP);
-    pinMode(WIO_MIC, INPUT);
     Serial.println("GPIO init completed.");
 #endif
+}
+
+// Interrupt Service Routine for Stopwatch Button
+void key_stopwatch_handler(void)
+{
+    key_stopwatch_pressed = true;
+}
+
+// Interrupt Service Routine for Scale Tare Button
+void key_scale_handler(void)
+{
+    key_scale_tare_pressed = true;
 }
